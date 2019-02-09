900db4880ec05bdf15d88fd7d9f712050eb4d6af
nfore <neilfore@gmail.com>
2019-02-09 12:47:30 -0600

    Default Android builds to OpenGL ES 3.0

--- android/phoenix64/jni/Application.mk
+++ android/phoenix64/jni/Application.mk
@@ -1,14 +1,15 @@
-ifeq ($(GLES),3)
-   ifndef NDK_GL_HEADER_VER
-      APP_PLATFORM := android-18
-   else
-      APP_PLATFORM := $(NDK_GL_HEADER_VER)
-   endif
-else
+ifeq ($(GLES),2)
    ifndef NDK_NO_GL_HEADER_VER
       APP_PLATFORM := android-9
    else
       APP_PLATFORM := $(NDK_NO_GL_HEADER_VER)
+   endif
+else  # default to OpenGL ES 3.0
+   GLES := 3
+   ifndef NDK_GL_HEADER_VER
+      APP_PLATFORM := android-18
+   else
+      APP_PLATFORM := $(NDK_GL_HEADER_VER)
    endif
 endif
 
