cec2b56bf315383002c1bfbe6d4ff4a5c11d37c9
nfore <neilfore@hotmail.com>
2022-01-01 16:59:56 -0600

    Remove obsolete workaround

--- command_event.c
+++ command_event.c
@@ -1155,7 +1155,6 @@ bool event_command(enum event_command cmd)
 #endif
          break;
       case EVENT_CMD_CORE_DEINIT:
-         event_command(EVENT_CMD_AUDIO_START);
          video_driver_free_hw_context();
 
          pretro_unload_game();
