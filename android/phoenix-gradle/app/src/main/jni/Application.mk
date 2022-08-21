a52ef5f601d0f3e86f9a3a3f303a860ac686b4a6
nfore <neilfore@hotmail.com>
2022-08-21 11:30:10 -0500

    (Android) a few build updates
    - androidx to v1.5
    - minSdkVersion to 21
    - compileSdkVersion to 32
    - gradle to v7.2.2
    - some cleanup

--- android/phoenix-gradle/app/src/main/jni/Application.mk
+++ android/phoenix-gradle/app/src/main/jni/Application.mk
@@ -1,16 +1,8 @@
 ifeq ($(GLES),2)
-   ifndef NDK_NO_GL_HEADER_VER
-      APP_PLATFORM := android-9
-   else
-      APP_PLATFORM := $(NDK_NO_GL_HEADER_VER)
-   endif
+   APP_PLATFORM := android-9
 else  # default to OpenGL ES 3.0
    GLES := 3
-   ifndef NDK_GL_HEADER_VER
-      APP_PLATFORM := android-18
-   else
-      APP_PLATFORM := $(NDK_GL_HEADER_VER)
-   endif
+   APP_PLATFORM := android-21
 endif
 
 ifndef TARGET_ABIS
