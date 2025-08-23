8297a54205b8eae3806c7cc6c28ab0a3a381de35
nfore <neilfore@hotmail.com>
2025-08-23 09:15:04 -0500

    Menu: Ensure paths are comparable when setting nav index to in-use path
    - Fixes "data/user/0" to "data/data" comparisons on Android

--- menu/menu_displaylist.c
+++ menu/menu_displaylist.c
@@ -1281,19 +1281,28 @@ static int menu_displaylist_parse_generic(menu_displaylist_info_t *info,
 }
 
 /* Returns nav index of @path, or nav index of the directory leading to it. */
-static unsigned menu_displaylist_path_nav_idx(file_list_t *list, char *path)
+static unsigned menu_displaylist_path_nav_idx(menu_displaylist_info_t *info,
+      char *path)
 {
-   menu_list_t *menu_list = menu_list_get_ptr();
-   const char *menu_dir   = NULL;
-   const char *menu_label = NULL;
+   file_list_t *list    = info->list;
+   const char *menu_dir = NULL;
    unsigned start, i;
 
-   menu_list_get_last_stack(menu_list, &menu_dir, &menu_label, NULL, NULL);
+   menu_list_get_last_stack(menu_list_get_ptr(), &menu_dir, NULL, NULL, NULL);
+
+   /* Ensure path and menu_dir are comparable */
+   strlcpy(info->path_b, path, PATH_MAX_LENGTH);
+   strlcpy(info->path_c, menu_dir, PATH_MAX_LENGTH);
+   path_resolve_realpath(info->path_b, PATH_MAX_LENGTH);
+   path_resolve_realpath(info->path_c, PATH_MAX_LENGTH);
+   path     = info->path_b;
+   menu_dir = info->path_c;
+
    if (strstr(path, menu_dir) != path)
       return 0;
 
    start = strlen(menu_dir) + 1;
-   if (path[start-1] == '\0')
+   if (path[start - 1] == '\0')
       return 0;
 
    /* Look for exact match. */
@@ -1368,11 +1377,7 @@ static void menu_displaylist_set_nav_to_path(menu_displaylist_info_t *info,
 
    if (path)
    {
-      if (path != info->path_b)
-         strlcpy(info->path_b, path, PATH_MAX_LENGTH);
-
-      path_resolve_realpath(info->path_b, PATH_MAX_LENGTH);
-      idx = menu_displaylist_path_nav_idx(info->list, info->path_b);
+      idx = menu_displaylist_path_nav_idx(info, path);
       if (idx != 0)
          menu_navigation_set(nav, idx, true);
    }
