6cab42ee382039e3b91dedc77ba8dbdbee7e5983
nfore <neilfore@hotmail.com>
2026-03-14 10:00:00 -0500

    Input Settings: Move remapping scope and auto keyboard focus to advanced

--- menu/menu_setting.c
+++ menu/menu_setting.c
@@ -6502,6 +6502,7 @@ static bool setting_append_list_input_options(
          1, true, true);
    (*list)[list_info->index - 1].get_string_representation = 
       &setting_get_string_representation_uint_scope_index;
+   settings_data_list_current_add_flags(list, list_info, SD_FLAG_ADVANCED);
 
    CONFIG_BOOL(
          settings->input.remap_binds_enable,
@@ -6569,6 +6570,7 @@ static bool setting_append_list_input_options(
          parent_group,
          general_write_handler,
          general_read_handler);
+   settings_data_list_current_add_flags(list, list_info, SD_FLAG_ADVANCED);
 
    END_SUB_GROUP(list, list_info, parent_group);
    START_SUB_GROUP(list, list_info, "Analog", group_info.name, subgroup_info, parent_group);
